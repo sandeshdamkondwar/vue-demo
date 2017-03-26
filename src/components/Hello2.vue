3207b78e9eb8c4e7b639b97547cce852986e5038
Sandesh Damkondwar <sandesh.da@zeta.tech>
2017-03-26 17:10:53 +0530

    Initial commit

---
+++ src/components/Hello2.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li v-for="link in ecosystemLinks">
+        <a v-bind:href="link.url">{{link.title}}</a>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'From new Component Hello2',
+      ecosystemLinks: [
+        {
+          url: 'http://router.vuejs.org/',
+          title: 'vue-router'
+        },
+        {
+          url: 'http://vuex.vuejs.org/',
+          title: 'vuex'
+        },
+        {
+          url: 'http://vue-loader.vuejs.org/',
+          title: 'vue-loader'
+        },
+        {
+          url: 'https://github.com/vuejs/awesome-vue',
+          title: 'awesome-vue'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Messages.vue
@@ -0,0 +1,118 @@
+<template>
+<div id="messages">
+	<div class="messages">
+		<div v-for="message in messages">
+			<div class="message" v-bind:class="{ left: message.source === 'from', right: message.source === 'to'}" >
+				<span v-html="message.subject"></span>
+			</div>
+		</div>
+	</div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'messages',
+  data () {
+    return {
+      messages: [
+        {
+          subject: 'Hey Sandesh, <br>How you are doing?',
+          time: new Date(),
+          source: 'from'
+        },
+        {
+          subject: 'Hey friend, I\'m doing great.',
+          time: new Date(),
+          source: 'to'
+        },
+        {
+          subject: 'Which framework is on mind today?',
+          time: new Date(),
+          source: 'from'
+        },
+        {
+          subject: 'Vue.js, killing it!!!',
+          time: new Date(),
+          source: 'to'
+        },
+        {
+          subject: 'Feels like, sitting on top of the world today <span>😂</span>',
+          time: new Date(),
+          source: 'to'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.messages {
+  border: 1px solid #DDD;
+  border-width: 1px 1px 2px 1px;
+  max-width: 400px;
+  margin: 20px 10px;
+  padding: 20px;
+
+}
+
+.message {
+	margin-bottom: 10px;
+}
+
+.message.right {
+	text-align: right;
+}
+
+.message.left {
+	text-align: left;
+}
+
+.message span {
+	display: inline-block;
+
+	padding: 10px;
+	color: white;
+    border-radius: 21px 21px 21px 21px;
+    position: relative;
+}
+
+
+
+.message span:after {
+    content: "";
+    position: absolute;
+    width: 10px;
+    height: 20px;
+
+    bottom: 0;
+    right: 0;
+    border-radius: 0 30% 0 290%;
+}
+
+.message.left span:after {
+	bottom: 0;
+    left: 0;
+    border-radius: 30% 0 290% 0 ;
+}
+
+.message.right span:after {
+	bottom: 0;
+    right: 0;
+    border-radius: 0 30% 0 290%;
+    background: rgb(23, 179, 50);
+}
+
+.message.left span,
+.message.left span:after {
+	background: #28384B;
+}
+
+.message.right span,
+.message.right span:after {
+	background: #36A36D;
+}
+
+</style>
